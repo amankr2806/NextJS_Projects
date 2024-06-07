88c82aa29b47fb77d98ed1297ffd92612fd78a0d
amankr2806 <amankumarbd@gmail.com>
2024-06-07 13:11:04 +0530

    corrected formatting

--- redux-counter/src/app/components/Button.js
+++ redux-counter/src/app/components/Button.js
@@ -2,29 +2,29 @@ import { useDispatch } from "react-redux";
 import { counterActions } from "../store";
 
 const Button = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const handleIncrement = () => {
-        //dispatch({type: "Increment"})
-        dispatch(counterActions.increment());
-    }
+  const handleIncrement = () => {
+    //dispatch({type: "Increment"})
+    dispatch(counterActions.increment());
+  };
 
-    const handleDecrement = () => {
-        //dispatch({type: "Decrement"})
-        dispatch(counterActions.decrement());
-    }
+  const handleDecrement = () => {
+    //dispatch({type: "Decrement"})
+    dispatch(counterActions.decrement());
+  };
 
-    const handleReset = () => {
-        //dispatch({type: "Reset"})
-        dispatch(counterActions.reset());
-    }
-    return(
-        <>
-        <button onClick={handleIncrement} >Increment</button>
-        <button onClick={handleDecrement} >Decrement</button>
-        <button onClick={handleReset} >ResetCount</button>
-        </>
-    )
-}
+  const handleReset = () => {
+    //dispatch({type: "Reset"})
+    dispatch(counterActions.reset());
+  };
+  return (
+    <>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
+      <button onClick={handleReset}>ResetCount</button>
+    </>
+  );
+};
 
 export default Button;
--- redux-counter/src/app/components/Header.js
+++ redux-counter/src/app/components/Header.js
@@ -1,7 +1,5 @@
 const Header = () => {
-    return(
-        <h1>Counter</h1>
-    )
-}
+  return <h1>Counter</h1>;
+};
 
 export default Header;
