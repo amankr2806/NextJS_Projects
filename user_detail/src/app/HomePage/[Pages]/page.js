53083e0fcff4ad146fa820c12f21d6e0ea0fa7e6
amankr2806 <amankumarbd@gmail.com>
2024-06-11 16:50:31 +0530

    displaying user detail

--- user_detail/src/app/HomePage/[Pages]/page.js
+++ user_detail/src/app/HomePage/[Pages]/page.js
@@ -1,9 +1,36 @@
-const UserDetail = () => {
-    return(
-        <>
-       <p>Hello</p> 
-        </>
-    );
-}
+"use client";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const UserDetail = ({ params }) => {
+  
+  const id = params.Pages;
+
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    if (id) {
+      axios
+        .get(`http://jsonplaceholder.typicode.com/users/${id}`)
+        .then((response) => {
+          setUser(response.data);
+        })
+        .catch((error) => {
+          console.error("Error fetching user data:", error);
+        });
+    }
+  }, [id]);
+
+  if (!user) return <div>Loading...</div>;
+
+  return (
+    <div>
+      <h1>{user.name}</h1>
+      <p>Email: {user.email}</p>
+      <p>Username: {user.username}</p>
+      {/* Render other user details here */}
+    </div>
+  );
+};
 
 export default UserDetail;
